b43685919e7dc197103204fa62e8da53168844db
侯栋 <houdong111@gmail.com>
2020-06-16 11:01:51 +0800

    add typings

---
+++ types/component.d.ts
@@ -0,0 +1,7 @@
+import Vue from 'vue'
+
+export declare class AgoraVueUIComponent extends Vue {
+  static install (vue: typeof Vue): void
+}
+
+export type AgoraVueUIComponentSize = 'lg' | 'md' | 'sm'
